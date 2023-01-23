b70183143c140315ee16e9ef3d54cbf11ae298f8
Snehal979 <bansodsnehal535@gmail.com>
2023-01-24 00:28:09 +0530

    [Snehal]Add: Welcome message For Player To Play Snake and Ladder Game

---
+++ SnakeAndLadderGame/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakeAndLadderGame
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Snake And Ladder Game ");
+            Console.ReadLine();
+        }
+    }
+}
