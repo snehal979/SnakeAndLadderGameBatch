1926c1c280a92308515cf5d7b96401a69a522da4
Snehal979 <bansodsnehal535@gmail.com>
2023-01-24 00:44:00 +0530

    [Snehal]Add: Starting Position Always Start From Zero

---
+++ SnakeAndLadderGame/GamePlay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame
+{
+    internal class GamePlay
+    {
+        int Player_Position = 0;
+        public void Starting_PositionOfPlayer()
+        { 
+            // Uc 1 Starting position 
+            Console.WriteLine("Let start the game");
+            Console.WriteLine("The Player is starting position "+Player_Position);
+        }
+    }
+}
--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -5,6 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Snake And Ladder Game ");
+            //Uc1 Starting position at zero
+            GamePlay gamePlay = new GamePlay();
+            gamePlay.Starting_PositionOfPlayer();
             Console.ReadLine();
         }
     }
