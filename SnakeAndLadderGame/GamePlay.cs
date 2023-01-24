f529125ea1c1b31ce4e910e99e4f97fd4997ea3d
Snehal979 <bansodsnehal535@gmail.com>
2023-01-24 23:34:27 +0530

    [Snehal]Add: Two Player game for Snake And Ladder

--- SnakeAndLadderGame/GamePlay.cs
+++ SnakeAndLadderGame/GamePlay.cs
@@ -11,16 +11,20 @@ namespace SnakeAndLadderGame
     internal class GamePlay
     {
         //Variable
-        int Player_Position = 0;
+        int Player_1_Position = 0, Player_2_Position = 0; // Vaiable for two player
         const int No_Play = 0, Ladder = 1, Snake = 2;
         int winning_Position =10;
         int Dice_Played_Count = 0;
+      
         Random random = new Random();
+       
         public void Starting_PositionOfPlayer()
-        { 
+        {
             // Uc 1 Starting position 
+            /// Uc7 For Two Player
             Console.WriteLine("Let start the game");
-            Console.WriteLine("The Player is starting position "+Player_Position);
+            Console.WriteLine("One Player is starting position "+Player_1_Position);
+            Console.WriteLine("Second Player is starting position "+Player_2_Position);
         }
         /// <summary>
         /// Uc2 DiceRoll
@@ -34,13 +38,25 @@ namespace SnakeAndLadderGame
             return diceRoll;
         }
         /// <summary>
+        /// Ture of Player Using Random Number
+        ///  /// Uc7 For Two Player
+        /// </summary>
+        /// <returns></returns>
+        public int PlayerTurn()
+        {
+            Random random = new Random();
+            int turnOfPlayer = random.Next(0, 2);
+            return turnOfPlayer;
+        }
+        /// <summary>
         /// Uc3 Choice Option for the no play,snake,ladder
         /// Uc4 Repeat till the Player reaches the winning position 100.
+        /// Uc7 For Two Player
         /// </summary>
         public void CheckOption()
         {
             string type = "";
-            while(Player_Position < winning_Position) 
+            while(Player_1_Position < winning_Position && Player_2_Position<winning_Position) 
             {
                 int option = random.Next(3);
                 switch (option)
@@ -49,24 +65,64 @@ namespace SnakeAndLadderGame
                         type ="No_Player";
                         break;
                     case Ladder:
-                        int DicePosition = DiceRoll(); // for the Current player position
-                        Player_Position += DicePosition;
-                        type ="Ladder";
-                        //Uc 5 When player position goes to greater than 100 then its automatically break the program 
-                        if (Player_Position > winning_Position) // not greater than 100 
+                        if(PlayerTurn() == 1)
                         {
-                            Console.WriteLine("Get Second Chance to Win");
-                            Player_Position -= DicePosition;
-                            break;
+                            //For Player one
+                            Console.WriteLine("one_player Play the game");
+                            int DicePosition = DiceRoll(); // for the Current player position
+                            Player_1_Position += DicePosition;
+                            type ="Ladder";
+                            
+                            //Uc 5 When player position goes to greater than 100 then its automatically break the program 
+                            if (Player_1_Position > winning_Position) // not greater than 100 
+                            {
+                                Console.WriteLine("Get Second Chance to Win");
+                                Player_1_Position -= DicePosition;
+                                break;
+                            }
                         }
+                        else
+                        {
+                            //For Player Second
+                            Console.WriteLine("Second_player Play the game");
+                            int DicePosition = DiceRoll(); // for the Current player position
+                            Player_2_Position += DicePosition;
+                            type ="Ladder";
+                           
+                            //Uc 5 When player position goes to greater than 100 then its automatically break the program 
+                            if (Player_2_Position > winning_Position) // not greater than 100 
+                            {
+                                Console.WriteLine("Get Second Chance to Win");
+                                Player_2_Position -= DicePosition;
+                                break;
+                            }
+                        }
+                        
                         break;
                     case Snake:
-                        Player_Position -= DiceRoll();
-                        type = "Snake";
-                        if(Player_Position < 0) // not less than zero
+                        if(PlayerTurn() == 1)
                         {
-                            Player_Position = 0;
+                            Console.WriteLine("one_player Play the game");
+                            //For Player ome
+                            Player_1_Position -= DiceRoll();
+                            type = "Snake";
+                            if (Player_1_Position < 0) // not less than zero
+                            {
+                                Player_1_Position = 0;
+                            }
                         }
+                        else
+                        {
+                            Console.WriteLine("Second_player Play the game");
+                            //For Player Second
+                            Player_2_Position -= DiceRoll();
+                            type = "Snake";
+                            if (Player_2_Position < 0) // not less than zero
+                            {
+                                Player_2_Position = 0;
+                            }
+                        }
+                        
                         break;
                     default:
                         Console.WriteLine("Invalid Selection");
@@ -74,16 +130,19 @@ namespace SnakeAndLadderGame
                        
                 }
                 Console.WriteLine("Type ="+type);
-                Console.WriteLine("The Player is current position = "+Player_Position);
+                Console.WriteLine("One Player is current position = "+Player_1_Position);
+                Console.WriteLine("Second Player is current position = "+Player_2_Position);
                 Console.WriteLine("---------");
-            }     
-            Console.WriteLine("Game over ! Player is at winning position {0} ", Player_Position);
+            }   
+            if(Player_1_Position == winning_Position)
+            Console.WriteLine("Game over ! one Player is at winning position {0} ", Player_1_Position);
+            else
+                Console.WriteLine("Game over ! second Player is at winning position {0} ", Player_2_Position);
 
             //Uc 6 Report the number of times the dice was played to win the game and also the position after every die role
             Console.WriteLine("Dice played Count for win the Game " + Dice_Played_Count);
 
         }
-
         
     }
 }
